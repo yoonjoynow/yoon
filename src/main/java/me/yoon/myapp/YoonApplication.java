ec068d2c69efed669f4bc5d5cf6783d510a5bb77
yoon <dbsehdghks45@naver.com>
2021-04-17 12:33:44 +0900

    최초 프로젝트 커밋

---
+++ src/main/java/me/yoon/myapp/YoonApplication.java
@@ -0,0 +1,13 @@
+package me.yoon.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoonApplication.class, args);
+    }
+
+}
